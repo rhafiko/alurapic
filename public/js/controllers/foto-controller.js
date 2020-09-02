544416fe7e4322321bc3c0759f6efee3f2670239
Najib El Alam <najib.alam@serpro.gov.br>
2020-09-01 23:02:31 -0300

    lesson 07 - handle new pic, form validation and submit to api by post rest

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,7 +1,20 @@
-angular.module("alurapic").controller("FotoController", function($scope) {
-  $scope.foto = [];
+angular
+  .module("alurapic")
+  .controller("FotoController", function($scope, $http) {
+    $scope.foto = {};
+    $scope.mensagem = "";
 
-  $scope.submeter = function() {
-    console.log($scope.foto);
-  };
-});
+    $scope.submeter = function() {
+      if ($scope.formulario.$valid) {
+        $http
+          .post("/v1/fotos", $scope.foto)
+          .success(function() {
+            $scope.foto = {};
+            $scope.mensagem = "Foto incluída com sucesso";
+          })
+          .error(function(erro) {
+            $scope.mensagem = "Não foi possível incluir a foto";
+          });
+      }
+    };
+  });
